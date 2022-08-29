2f13c299e719d0d7764c24e5deb958848d409e29
Topo81 <103972679+Topo81@users.noreply.github.com>
2022-08-29 17:13:14 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var nombre= "Tomas"
+var apellido= "Biasutti"
+var persona={
+    nombre:"Tomas",
+    apellido:"Biasutti"
+}
+sessionStorage.setItem("persona",JSON.stringify())
+localStorage.setItem("persona",JSON.stringify())
+document.cookie="PersonaCaducidad= Persona;expires= max-age="+ 00*02*00*00;
